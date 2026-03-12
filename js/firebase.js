7e1bfff9e126228713b5375bd9926991940cbc63
OmerGilboaPc <omergilboapc@gmail.com>
2026-03-12 03:47:52 +0200

    Add files via upload

--- js/firebase.js
+++ js/firebase.js
@@ -1,22 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCPDS6U9LokVN-f4uQj9rdaWuCnut72bts",
   authDomain: "netflixfamilystreaming-b0ca4.firebaseapp.com",
-  databaseURL: "https://netflixfamilystreaming-b0ca4-default-rtdb.firebaseio.com",
+  databaseURL: "https://netflixfamilystreaming-b0ca4-default-rtdb.firebaseio.com/",
   projectId: "netflixfamilystreaming-b0ca4",
   storageBucket: "netflixfamilystreaming-b0ca4.firebasestorage.app",
   messagingSenderId: "116100612969",
   appId: "1:116100612969:web:29387c89e455e36d8373f8"
 };
 
-
-// אתחול האפליקציה
-const app = initializeApp(firebaseConfig);
+export const ADMIN_EMAIL = "omergilboapc@gmail.com";
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
-export const ADMIN_EMAIL = "omergilboapc@gmail.com";
